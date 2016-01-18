3229af9519304d036e41cad28dc3a0b507c23f3f
cstuncsik <csaba.tuncsik@gmail.com>
2016-01-18 16:06:03 +0100

    removing TexturePacker generated meta property from .json in production

--- tasks/prod/data.js
+++ tasks/prod/data.js
@@ -2,10 +2,12 @@ import config from '../config.js';
 import gulp from 'gulp';
 import util from 'gulp-util';
 import jsonMinify from 'gulp-json-minify';
+import replace from 'gulp-replace';
 
 gulp.task('data:prod', () => {
     return gulp.src(config.paths.src.data)
         .pipe(jsonMinify())
+        .pipe(replace(/,"meta":\{.*}$/, '}'))
         .pipe(gulp.dest(config.paths.builds.tmp.data))
         .on('error', util.log);
 });
